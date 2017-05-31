99fc8bf3ca2839fb73607318196b504665fb535f
Yurii Serediuk <yserediuk@n-ix.com>
2017-05-31 21:17:15 +0300

    Setup and load map fragment.

--- app/src/main/java/com/ysered/savemylocationsample/MapActivity.kt
+++ app/src/main/java/com/ysered/savemylocationsample/MapActivity.kt
@@ -2,11 +2,28 @@ package com.ysered.savemylocationsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapActivity : AppCompatActivity() {
+@SuppressWarnings("MissingPermission")
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let {
+            // TODO: setOnMapClickListener
+            // TODO: setOnMarkerClickListener
+            // TODO: setOnMarkerDragListener
+            it.isMyLocationEnabled = true
+            it.uiSettings.isMapToolbarEnabled = false
+        }
     }
 }
