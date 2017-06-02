40da93d46fc5995fd6d200a165d89f290eedd3df
Yurii Serediuk <yserediuk@n-ix.com>
2017-06-02 17:01:16 +0300

    Inline Activity extension functions for better performance.

--- app/src/main/java/com/ysered/savemylocationsample/util/PermissionUtils.kt
+++ app/src/main/java/com/ysered/savemylocationsample/util/PermissionUtils.kt
@@ -12,7 +12,7 @@ import android.support.v4.app.ActivityCompat
  * @param [requestCode] to filter results in [Activity.onRequestPermissionsResult]
  * @param [onGranted] function to be executed if permissions already granted
  */
-fun Activity.requestLocationPermissionsIfNeeded(requestCode: Int, onGranted: () -> Unit): Unit {
+inline fun Activity.requestLocationPermissionsIfNeeded(requestCode: Int, onGranted: () -> Unit): Unit {
     requestPermissionsIfNeeded(requestCode,
             onGranted,
             Manifest.permission.ACCESS_FINE_LOCATION,
@@ -26,7 +26,7 @@ fun Activity.requestLocationPermissionsIfNeeded(requestCode: Int, onGranted: () 
  * @param [onGranted] function to be executed if permissions already granted
  * @param [permissions] array of permission to be requested if some of them aren't grated
  */
-fun Activity.requestPermissionsIfNeeded(requestCode: Int, onGranted: () -> Unit, vararg permissions: String): Unit {
+inline fun Activity.requestPermissionsIfNeeded(requestCode: Int, onGranted: () -> Unit, vararg permissions: String): Unit {
     val toRequest = permissions
             .filterNot { ActivityCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED }
             .toTypedArray()
@@ -45,10 +45,10 @@ fun Activity.requestPermissionsIfNeeded(requestCode: Int, onGranted: () -> Unit,
  * @param [onGranted] function to be executed if all permissions is granted
  * @param [onDenied] function to be executed if some or all permissions is denied
  */
-fun Activity.processPermissionResults(permissions: Array<out String>,
-                                      grantResults: IntArray,
-                                      onGranted: () -> Unit,
-                                      onDenied: (Boolean) -> Unit) {
+inline fun Activity.processPermissionResults(permissions: Array<out String>,
+                                             grantResults: IntArray,
+                                             onGranted: () -> Unit,
+                                             onDenied: (Boolean) -> Unit) {
     if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
         onGranted()
     } else {
