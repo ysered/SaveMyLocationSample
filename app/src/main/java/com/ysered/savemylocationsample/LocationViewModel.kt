2bda3a1bac9a9ab3e961e1a7f166a9b8307719b3
Yurii Serediuk <yserediuk@n-ix.com>
2017-06-02 18:09:12 +0300

    Add marker marker on click.

--- app/src/main/java/com/ysered/savemylocationsample/LocationViewModel.kt
+++ app/src/main/java/com/ysered/savemylocationsample/LocationViewModel.kt
@@ -2,8 +2,11 @@ package com.ysered.savemylocationsample
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.MutableLiveData
+import com.google.android.gms.maps.model.LatLng
 
 class LocationViewModel(application: Application) : AndroidViewModel(application) {
 
-    val location: LocationLiveData = LocationLiveData(application.applicationContext)
+    val locationUpdates = LocationUpdatesLiveData(application.applicationContext)
+    val latLng = MutableLiveData<LatLng>()
 }
--- app/src/main/java/com/ysered/savemylocationsample/MapActivity.kt
+++ app/src/main/java/com/ysered/savemylocationsample/MapActivity.kt
@@ -3,14 +3,13 @@ package com.ysered.savemylocationsample
 import android.arch.lifecycle.LifecycleActivity
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-import android.location.Location
 import android.os.Bundle
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.ysered.savemylocationsample.util.debug
+import com.google.android.gms.maps.model.MarkerOptions
 import com.ysered.savemylocationsample.util.processPermissionResults
 import com.ysered.savemylocationsample.util.requestLocationPermissionsIfNeeded
 import com.ysered.savemylocationsample.util.showToast
@@ -19,53 +18,56 @@ import com.ysered.savemylocationsample.util.showToast
 class MapActivity : LifecycleActivity(), OnMapReadyCallback {
 
     private val LOCATION_PERMISSION_REQUEST = 1
-    private val CAMERA_ZOOM = 16f
+    private val CAMERA_ZOOM = 18f
 
-    private val mapFragment by lazy { supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment }
-    private val locationViewModel: LocationViewModel by lazy { ViewModelProviders.of(this).get(LocationViewModel::class.java) }
-    private lateinit var googleMap: GoogleMap
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var locationViewModel: LocationViewModel
+
+    private val initMap: () -> Unit = { mapFragment.getMapAsync(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        requestLocationPermissionsIfNeeded(LOCATION_PERMISSION_REQUEST, onGranted = { initMap() })
+        mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        locationViewModel = ViewModelProviders.of(this).get(LocationViewModel::class.java)
+
+        requestLocationPermissionsIfNeeded(LOCATION_PERMISSION_REQUEST, initMap)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when (requestCode) {
-            LOCATION_PERMISSION_REQUEST ->
-                processPermissionResults(permissions, grantResults,
-                        onGranted = { initMap() },
-                        onDenied = { showToast(R.string.enable_location_permission) })
+            LOCATION_PERMISSION_REQUEST -> processPermissionResults(permissions, grantResults,
+                    onGranted = initMap,
+                    onDenied = { showToast(R.string.enable_location_permission) })
             else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
         googleMap?.let {
-            // TODO: setOnMapClickListener
+            it.setOnMapClickListener { locationViewModel.latLng.value = it }
             // TODO: setOnMarkerClickListener
             // TODO: setOnMarkerDragListener
             it.isMyLocationEnabled = true
             it.uiSettings.isMapToolbarEnabled = false
             it.uiSettings.isMyLocationButtonEnabled = false
-
-            locationViewModel.location.observe(this, Observer { location ->
-                debug("Observed location update: $location")
-                location?.let { moveCamera(googleMap, it) }
-            })
-            this.googleMap = it
+            bindObservers(it)
         }
     }
 
-    private fun initMap() {
-        mapFragment.getMapAsync(this)
-    }
-
-    private fun moveCamera(googleMap: GoogleMap, location: Location) {
-        val current = LatLng(location.latitude, location.longitude)
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(current))
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current, CAMERA_ZOOM))
+    private fun bindObservers(googleMap: GoogleMap) {
+        locationViewModel.locationUpdates.observe(this, Observer { location ->
+            location?.let { // move camera
+                val current = LatLng(location.latitude, location.longitude)
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(current))
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current, CAMERA_ZOOM))
+            }
+        })
+        locationViewModel.latLng.observe(this, Observer { myLocationEntity ->
+            myLocationEntity?.let { // add marker
+                googleMap.addMarker(MarkerOptions().position(it))
+            }
+        })
     }
 }
