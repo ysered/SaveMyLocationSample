9724b8e9edc096592329094772eea26d3c3a16cf
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-06-04 00:12:02 +0300

    Remove unused val declaration.

--- app/src/main/java/com/ysered/savemylocationsample/MapActivity.kt
+++ app/src/main/java/com/ysered/savemylocationsample/MapActivity.kt
@@ -18,7 +18,6 @@ import com.ysered.extension.showToast
 class MapActivity : LifecycleActivity(), OnMapReadyCallback {
 
     private val LOCATION_PERMISSION_REQUEST = 1
-    private val CAMERA_ZOOM = 16f
 
     private lateinit var mapFragment: SupportMapFragment
     private lateinit var mapViewModel: MapViewModel
